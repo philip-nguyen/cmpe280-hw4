49671c295bc64b2af8f518a4a134f9cff468d51b
Philip <philip.nguyen@sjsu.edu>
2021-04-04 16:35:01 -0700

    mysql inserts working

--- app.js
+++ app.js
@@ -7,10 +7,22 @@ var ejs = require('ejs');
 var mysql = require('mysql');
 var path = require('path');
 var bodyParser = require('body-parser');
+var $ = require('jquery');
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 // place html and css in public folder to be
 // rendered together
 app.use(express.static("public"));
 
+//set the path of the jquery file to be used from the node_module jquery package  
+app.use('/jquery',express.static(path.join(__dirname+'/node_modules/jquery/dist/')));  
+
+// mysql connection
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -22,6 +34,58 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
 });
+
+
+app.post("", jsonParser, function(req, res) {
+    // get the request body
+    console.log("Req body: ", req.body);
+
+    if(req.body.fname) {
+        var name = req.body.fname + " " + req.body.lname;
+        var gender = req.body.gender;
+        var age = req.body.age;
+        var photo = req.body.photo;
+        
+        connection.query('INSERT INTO Patients (name, gender, age, photo) VALUES(?, ?, ?, ?)',
+                            [name, gender, age, photo]);
+        
+        connection.query('SELECT id FROM Patients WHERE name = '+quoteString(name)+' \
+            AND gender = '+quoteString(gender)+' AND age = '+quoteString(age), 
+            (err, rows) => {
+                console.log("ID: ", rows[0].id);
+                
+                //sessionStorage.setItem('curId', results.rows[0].id);
+                res.json(rows[0].id);
+            });
+    }
+    
+    if(req.body.medications) {
+        var meds = req.body.medications;
+        var notes = req.body.notes;
+        var id = req.body.id;
+        connection.query('UPDATE Patients SET medications=?, notes=? WHERE id = ?',
+                        [meds, notes, id]);
+
+        connection.query('SELECT * FROM Patients', function(err, rows, fields) {
+            if (err) throw err;
+            var data = [];
+            for(i = 0; i < rows; i++) {
+                data.push(rows[i].name);
+                data.push(rows[i].age);
+                data.push(rows[i].gender);
+                data.push(rows[i].photo);
+                data.push(rows[i].medications);
+                data.push(rows[i].notes);
+            }
+            res.json(JSON.stringify(data));
+        });
+    }
+    
+});
+
+
+
+
 
 app.post('/demo', function(req, res) {
     event.preventDefault();
@@ -38,129 +102,7 @@ app.post('/demo', function(req, res) {
 app.listen(8000);
 console.log('Example app listening at port:8000');
 
-// Server functions 
-// function to save form for demographics
-function saveDemographics() {
-    console.log("Save Demographics Call");
-
-    event.preventDefault();
-    var fname = document.getElementById("fname").value;
-    var lname = document.getElementById("lname").value;
-    var gender = document.getElementById("gender").value;
-    var age = document.getElementById("age").value;
-    var notes = document.getElementById("other").value;
-    var photo = document.getElementById("photo") ? 
-                document.getElementById("photo").value.substring(12) : null;
-    // var photo = photo.substring(12);
-    // console.log(photo.substring(12));
-    // set all the variables
-    const mysql = require('mysql');
-    var connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'health_spa'
-        });
-    connection.connect((err) => {
-        if (err) throw err;
-        console.log('Connected!');
-        });
-
-    connection.query('INSERT INTO Patients (name, gender, age, photo) VALUES(?, ?, ?, ?)',
-                        [name, gender, age, photo]);
-    connection.query('SELECT id FROM Patients WHERE name = '+quoteString(name)+' \
-        AND gender = '+quoteString(gender)+' AND age = '+quoteString(age), 
-        (err, rows) => {
-            console.log(rows[0].id);
-            
-            sessionStorage.setItem('curId', results.rows[0].id);
-        });
-    
-    var demos = document.getElementById("demographics");
-    var vitals = document.getElementById("vitals");
-    demos.style.display = "none";
-    vitals.style.display = "block";
-
-    //window.location.assign("vitals.html");
-}
-// function to save form for health vitals
-function saveVitals() {
-    console.log("Save Health Vitals Call");
-
-    event.preventDefault();
-    var height = document.getElementById("height").value;
-    var weight = document.getElementById("weight").value;
-    var bTemp = document.getElementById("bTemp").value;
-    var pulse = document.getElementById("pulse").value;
-    var bp = document.getElementById("bp").value;
-    var meds = document.getElementById("medications").value;
-    var notes = document.getElementById("notes").value;
-
-    
-    connection.query('UPDATE Patients SET medications=?, notes=? WHERE id = ?',
-                            [meds, notes, sessionStorage.getItem('curId') ]);
-
-    var report = document.getElementById("report");
-    var vitals = document.getElementById("vitals");
-    report.style.display = "block";
-    vitals.style.display = "none";
-    showReport();
-    //window.location.assign("");
-}
-// function to read from DB
-function showReport() {
-
-    var db = openDatabase('healthSpa', '1.0', 'HealthSpa DB', 2 * 1024 * 1024);
-
-    db.transaction(function (tx) { 
-        tx.executeSql('SELECT * FROM Patients', [], function (tx, results) { 
-           var len = results.rows.length, i; 
-           //msg = "<p>Found rows: " + len + "</p>"; 
-           //document.querySelector('#status').innerHTML +=  msg; 
-            var report = document.getElementById("report");
-            
-           for (i = 0; i < len; i++) { 
-              //msg = "<p><b>" + results.rows.item(i).log + "</b></p>"; 
-              //document.querySelector('#status').innerHTML +=  msg;
-               var patientRow = document.createElement("tr");
-
-               var patientDetail = document.createElement("td");
-               var detail = document.createTextNode(results.rows[i].name);
-               patientDetail.appendChild(detail);
-               patientRow.appendChild(patientDetail);
-
-               var patientDetail = document.createElement("td");
-               var detail = document.createTextNode(results.rows[i].age);
-               patientDetail.appendChild(detail);
-               patientRow.appendChild(patientDetail);
-
-               var patientDetail = document.createElement("td");
-               var detail = document.createTextNode(results.rows[i].gender);
-               patientDetail.appendChild(detail);
-               patientRow.appendChild(patientDetail);
-
-               var patientDetail = document.createElement("td");
-               var detail = document.createTextNode(results.rows[i].photo);
-               patientDetail.appendChild(detail);
-               patientRow.appendChild(patientDetail);
-
-               var patientDetail = document.createElement("td");
-               var detail = document.createTextNode(results.rows[i].medications);
-               patientDetail.appendChild(detail);
-               patientRow.appendChild(patientDetail);
-
-               var patientDetail = document.createElement("td");
-               var detail = document.createTextNode(results.rows[i].notes);
-               patientDetail.appendChild(detail);
-               patientRow.appendChild(patientDetail);
-
-               report.appendChild(patientRow);
-           } 
-        }, null); 
-     }); 
-}
-// function to access camera and take picture???
-
+// quote string function
 function quoteString(string) {
     return '"' + string + '"';
 }
